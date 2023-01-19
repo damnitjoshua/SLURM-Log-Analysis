d19ead960017ee90070caa54e0632b98bfcbe369
Kairos5106 <kevinhotzewan@gmail.com>
2023-01-19 14:49:58 +0800

    test 2

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test{
     public static void main(String[] args){
-        System.out.printf("Hello World! %s", "I am not gay btw");
+        System.out.printf("Hello World! %s\n", "I am not gay btw");
+        int number = 17;
+        System.out.println(number);
     }
 }
