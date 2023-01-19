7df5777258fdee9204640b503290a8bc5b82549b
Kairos5106 <kevinhotzewan@gmail.com>
2023-01-19 17:08:10 +0800

    cleaning up branch

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test{
-    public static void main(String[] args){
-        System.out.printf("Hello World! %s\n", "I am not gay btw");
-        int number = 17;
-        System.out.println(number);
-    }
-}
